6fde8a1ba09ee60b9e004781e750fcb94a658d76
Andy Byers <ajrbyers@gmail.com>
2018-11-02 10:06:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from setuptools import find_packages, setup
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
 
-kwargs = {
-    'name': 'paperbuzz',
-    'version': '0.1',
-    'description': "A django application for Paperbuzz metrics.",
-    'keywords': 'Open Library of Humanities Paperbuzz PKP',
-    'author': 'Andy Byers',
-    'author_email': 'tech@openlibhums.org',
-    'url': 'https://github.com/birkbeckctp/django-paperbuzz',
-    'license': 'MIT',
-    'packages': ['paperbuzz'],
-    'zip_safe': False,
-    'install_requires': [],
-}
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-setup(**kwargs)
+setup(
+    name='django-paperbuzz',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License', 
+    description='A simple Django app to display paperbuzz metrics.',
+    long_description=README,
+    url='https://github.com/birkbeckctp/django-paperbuzz',
+    author='Andy Byers',
+    author_email='andy.byers@openlibhums.org',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.X', 
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', 
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
